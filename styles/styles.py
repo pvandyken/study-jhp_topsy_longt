a0cd74edf46f615833ad9f5f3964e96ddf51eb67
Peter Van Dyken <pvandyk2@uwo.ca>
2024-09-11 19:15:40 -0400

    Updates for new submission

--- styles/styles.py
+++ styles/styles.py
@@ -1,9 +1,11 @@
 
-panel_label = dict(size=20, weight="bold", color="#303030")
-col_title = dict(fontdict=dict(weight="bold", color="#303030", size=14))
+LARGE = 7
+
+panel_label = dict(fontdict=dict(fontsize=7, weight="bold"), color="#303030")
+col_title = dict(fontdict=dict(weight="bold", color="#303030", size=LARGE), size=LARGE, weight="bold")
 subfig_title = dict(color="#303030")
 colorbar_label = dict(fontsize=10)
-row_title = dict(ha="right", va="center", rotation="vertical", size=14, color="#101010")
+row_title = dict(ha="right", va="center", rotation="vertical", size=LARGE, color="#101010")
 
 
 
